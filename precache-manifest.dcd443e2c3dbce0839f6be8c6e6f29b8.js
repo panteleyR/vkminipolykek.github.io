a39db43049bc5595c9760bdf8392fb9c4e3a18fa
panteleyR <panteley98@bk.ru>
2020-12-03 23:13:00 +0300

    Updates

---
+++ precache-manifest.dcd443e2c3dbce0839f6be8c6e6f29b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593166faa862b9f53cea293be9a639fa",
+    "url": "/vkminipolykek/index.html"
+  },
+  {
+    "revision": "7ce74ae41b5c00f701b4",
+    "url": "/vkminipolykek/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "dcfaa5dc0721ee32b9ca",
+    "url": "/vkminipolykek/static/css/main.d36085b4.chunk.css"
+  },
+  {
+    "revision": "7ce74ae41b5c00f701b4",
+    "url": "/vkminipolykek/static/js/2.6464ad24.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vkminipolykek/static/js/2.6464ad24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfaa5dc0721ee32b9ca",
+    "url": "/vkminipolykek/static/js/main.85753739.chunk.js"
+  },
+  {
+    "revision": "451b05dcfb0116aa1e49",
+    "url": "/vkminipolykek/static/js/runtime-main.caf43e52.js"
+  },
+  {
+    "revision": "6ae1d7ac0dbbc02e6dc825abf333de0d",
+    "url": "/vkminipolykek/static/media/favicon.6ae1d7ac.ico"
+  },
+  {
+    "revision": "33065b470e036fe852943ab272536550",
+    "url": "/vkminipolykek/static/media/fit_banner.33065b47.jpg"
+  },
+  {
+    "revision": "312a53b684eb0b9ba940bf98886c65c3",
+    "url": "/vkminipolykek/static/media/mospolytech-logo-hero-white.312a53b6.png"
+  },
+  {
+    "revision": "ab7f61de97ef03fb45682482c5b5404c",
+    "url": "/vkminipolykek/static/media/mospolytech-logo-white.ab7f61de.svg"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminipolykek/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "f7354f82f6ed86fd45fd637f6804ae09",
+    "url": "/vkminipolykek/static/media/spiral.f7354f82.png"
+  }
+]);
